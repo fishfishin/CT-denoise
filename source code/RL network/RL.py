68a44a5dbac37ac169d283c9f6784a61f65ff0f0
fishfishin <54790913+fishfishin@users.noreply.github.com>
2020-10-22 01:54:17 +0200

    Add files via upload

--- source code/RL network/RL.py
+++ source code/RL network/RL.py
@@ -12,7 +12,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+import torchvision.transforms as T
 
+
+env = gym.make('CartPole-v0').unwrapped
+
+# set up matplotlib
 is_ipython = 'inline' in matplotlib.get_backend()
 if is_ipython:
     from IPython import display
@@ -24,32 +29,91 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward'))
-                        
-                        
-class DQN(nn.Module):
+action = namedtuple('action',
+                        ('+0.5', '+0.1', '0', '-0.1', '-0.5'))
+para = namedtuple('parameter',
+                        ('sigma', 'beta_kasier'))
 
-    def __init__(self, h, w, outputs):
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+class doubleDQN(nn.Module):
+
+    def __init__(self, h, w, output1,output2):
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(32)
 
-        # Number of Linear input connections depends on output of conv2d layers
-        # and therefore the input image size, so compute it.
-        def conv2d_size_out(size, kernel_size = 5, stride = 2):
+        # image patch size is   h x w
+        #first two  shared conv layers
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=2)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        # patch size after conv layer
+        def conv2d_size_out(size, kernel_size = 3, stride = 2):
             return (size - (kernel_size - 1) - 1) // stride  + 1
-        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
-        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
-        linear_input_size = convw * convh * 32
-        self.head = nn.Linear(linear_input_size, outputs)
+        convw = conv2d_size_out(conv2d_size_out(w))
+        convh = conv2d_size_out(conv2d_size_out(h))
+        linear_input_size = convw * convh * 64
 
-    # Called with either one element to determine next action, or a batch
-    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+        #  Parameter selection
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.dense1 = nn.Linear(linear_input_size, 128)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.head1 = nn.Linear(128,output1)
+
+        linear_input_size = convw * convh * 128
+        #  Parameter tuning
+        self.conv4 = nn.Conv2d(64, 128, kernel_size=3, stride=1)
+        self.bn5 = nn.BatchNorm2d(128)
+        self.conv5 = nn.Conv2d(128, 128, kernel_size=3, stride=1)
+        self.bn6 = nn.BatchNorm2d(128)
+        self.dense2 = nn.Linear(linear_input_size, 256)
+        self.bn7 = nn.BatchNorm2d(256)
+        self.head2 = nn.Linear(256, output2)
+
+    
     def forward(self, x):
+
+        # 2 shared layers
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        return self.head(x.view(x.size(0), -1))
+
+        #  Parameter selection
+        out1 = F.relu(self.bn3(self.conv3(x)))
+        out1 = out1.view(out1.size(0), -1)
+        out1 = F.relu(self.bn4(self.dense1(out1)))
+        out1 = self.head1(out1)
+
+        #  Parameter tuning
+        out2 = F.relu(self.bn5(self.conv4(x)))
+        out2 = F.relu(self.bn6(self.conv5(out2)))
+        out2 = out2.view(out2.size(0), -1)
+        out2 = F.relu(self.bn7(self.dense2(out2)))
+        out2 = self.head2(out2)
+
+        return  out1, out2
+
+
+
+
+
+
