96fe39a37ac21bdf74f8c8c9c0c93d64f590953b
fishfishin <54790913+fishfishin@users.noreply.github.com>
2020-10-23 07:57:45 +0200

    Update NLM.py

--- source code/NLMF/NLM.py
+++ source code/NLMF/NLM.py
@@ -8,13 +8,14 @@ import numpy as np
 from skimage import io,img_as_float,img_as_ubyte
 from skimage.restoration import denoise_nl_means, estimate_sigma
 from PIL import Image
+import matplotlib.pyplot as plt
 
 def NLM(img):
     
     sigma_est = np.mean(estimate_sigma(img,multichannel=False))
 
     Final_img =denoise_nl_means(img, h =1.15*sigma_est,fast_mode=True,patch_size=5,patch_distance=3,multichannel=False)
-    psnr = PSNR.PSNR(img, denoised_img)
+    psnr = PSNR.PSNR(img, Final_img)
     print ("The PSNR between the two img after NLMF is %f" % psnr)
     fig, axs = plt.subplots(1, 2)
     fig.suptitle('NLM')
