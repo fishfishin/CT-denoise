2031ddc0b267fc069822fa867d4fe84431863524
fishfishin <54790913+fishfishin@users.noreply.github.com>
2020-10-23 07:54:43 +0200

    Add files via upload

--- source code/NLMF/NLM.py
+++ source code/NLMF/NLM.py
@@ -1,7 +1,7 @@
 """""
 Non locl means
 """
-
+import skimage
 from PIL import  PSNR
 import cv2
 import numpy as np
@@ -9,20 +9,33 @@ from skimage import io,img_as_float,img_as_ubyte
 from skimage.restoration import denoise_nl_means, estimate_sigma
 from PIL import Image
 
-img = io.imread("C:/Users/ZhenjuYin/Downloads/a1.tif", as_gray=False)
-img = img[5,:,:]
-img = img_as_float( img )
-#img_name = "C:/Users/ZhenjuYin/Downloads/a1.tif" 
-#img = Image.open(img_name)
-#img = np.array(img)
-sigma_est = np.mean(estimate_sigma(img,multichannel=False))
+def NLM(img):
+    
+    sigma_est = np.mean(estimate_sigma(img,multichannel=False))
 
-denoised_img =denoise_nl_means(img, h =1.15*sigma_est,fast_mode=True,patch_size=5,patch_distance=3,multichannel=False)
-psnr = PSNR.PSNR(img, denoised_img)
-print ("The PSNR between the two img after NLMF is %f" % psnr)
-#img_as_8 = img_as_ubyte(denoised_img)
-#final_img = cv2.cvtColor(img_as_8,cv2.COLOR_BGR2RGB)
-cv2.imwrite('NLM.jpg',denoised_img)
-#img_as_8 = img_as_ubyte(img)
-#final_img = cv2.cvtColor(img_as_8,cv2.COLOR_BGR2RGB)
-cv2.imwrite('NLM_o.jpg',img)
+    Final_img =denoise_nl_means(img, h =1.15*sigma_est,fast_mode=True,patch_size=5,patch_distance=3,multichannel=False)
+    psnr = PSNR.PSNR(img, denoised_img)
+    print ("The PSNR between the two img after NLMF is %f" % psnr)
+    fig, axs = plt.subplots(1, 2)
+    fig.suptitle('NLM')
+    axs[ 0].imshow(img, cmap='gray')
+    axs[ 1].imshow(Final_img, cmap='gray')
+    axs[ 0].set_title('original')
+    axs[ 1].set_title('final')
+
+    plt.show()
+
+    return Final_img
+
+
+
+if __name__ == '__main__':
+    image = skimage.external.tifffile.imread("C:/Users/ZhenjuYin/Downloads/a1.tif")
+
+    
+    for i in range(16):
+        img = image[i,:,:]
+        image[i,:,:] = NLM(img)
+    skimage.external.tifffile.imsave('t.tif', image)
+    image = skimage.external.tifffile.imread("C:/Users/ZhenjuYin/Downloads/t.tif")
+    skimage.external.tifffile.imshow(image[15,:,:],photometric='miniswhite' )
