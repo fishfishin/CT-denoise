672ec3ac6419256e809e7daa8a6b9360176ca25a
fishfishin <54790913+fishfishin@users.noreply.github.com>
2020-10-26 16:42:17 +0100

    Add files via upload

--- source code/RL network/trainRL.py
+++ source code/RL network/trainRL.py
@@ -14,15 +14,21 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
-from PIL import Bm3d
+from PIL import Bm3d,NLM
 import skimage
+import copy
 
+if torch.cuda.is_available():  
+  dev = "cuda:0" 
+else:  
+  dev = "cpu"  
+print(dev)
+device = torch.device(dev)  
+a = torch.zeros(4,3)    
+a = a.to(device)
 
-
-Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
 ####  1st path
-Para = ['sigma', 'beta_kasier']
+Para = ['sigma', 'size','dist']
 sigma = 1
 Beta_Kaiser = 2.0
 
@@ -37,29 +43,32 @@ Patch_num = RESOLUTION **2
 PATCH_reward = 5
 TARGET_UPDATE_STEP = 300
 MAXSTEPS_BM3D = 20
-REPLAY_MEMORY = 3200   ######### buffer？
+REPLAY_MEMORY = 5000000  ######### buffer？
 BATCH_SIZE = 128
 
 TUNING_STEP= 20
 
 def replay_train(mainDQN: RL.doubleDQN, targetDQN: RL.doubleDQN, states, next_states, action,parameter, rewards) -> np.float64:
-    X = states
-    X1 = next_states
+
+    X = torch.zeros(states.shape[0], 1, PATCH_SIZE[0],PATCH_SIZE[0])
+    X1 = torch.zeros(states.shape[0], 1, PATCH_SIZE[0],PATCH_SIZE[0])
+    X[:,0,:,:] = torch.from_numpy(states)
+    X1[:,0,:,:] = torch.from_numpy(next_states)
 
     t1, t2 = targetDQN(X1)  #### old version of NET
     y1, y2 = mainDQN(X)
 
     ####  1st path
-    temp = torch.max(t1, axis=1)
-    Q_target = rewards + DISCOUNT_RATE * temp
-    
+    tem = torch.max(t1, axis=1)
+    temp = tem.values
+    Q_target = rewards + DISCOUNT_RATE * temp.detach().numpy()
     for i in range(y1.shape[0]):
         y1[i,int(parameter[i])] = Q_target[i]
 
     ####  2nd path
-    temp = torch.max(t2, axis=1)
-    Q_target = rewards + DISCOUNT_RATE * temp
-    
+    tem = torch.max(t2, axis=1)
+    temp = tem.values
+    Q_target = rewards + DISCOUNT_RATE * temp.detach().numpy()
     for i in range(y2.shape[0]):
         y2[i,int(action[i])] = Q_target[i]
 
@@ -67,9 +76,9 @@ def replay_train(mainDQN: RL.doubleDQN, targetDQN: RL.doubleDQN, states, next_st
 
 
 
-def divid_patch(fimg) -> np.float64:
+def divide_patch(fimg) -> np.float64:
 
-    fimgpad = np.zeros((RESOLUTION + PATCH_SIZE[0] -1, RESOLUTION + PATCH_SIZE[0] -1))
+    fimgpad = np.zeros((RESOLUTION + PATCH_SIZE[0] -1, RESOLUTION + PATCH_SIZE[0] -1),dtype =np.float64)
     fimgpad[int((PATCH_SIZE[0]+1)/2)-1:RESOLUTION+int((PATCH_SIZE[0]+1)/2)-1,int((PATCH_SIZE[0]+1)/2)-1:RESOLUTION+int((PATCH_SIZE[0]+1)/2)-1]=fimg
     state = np.zeros((Patch_num,PATCH_SIZE[0], PATCH_SIZE[0]))
     count = 0
@@ -105,28 +114,41 @@ def Denoise(state, parameter, action, parameter_value, GroundTruth, original_ima
                 parameter_value[idx,1] = parameter_value[idx,1]*0.9
             if action[idx]==4:
                 parameter_value[idx,1] = parameter_value[idx,1] *0.5
+        if parameter[idx]==2:
+            if action[idx]==0:
+                parameter_value[idx,2] = parameter_value[idx,1] *1.5
+            if action[idx]==1:
+                parameter_value[idx,2] = parameter_value[idx,1] *1.1
+            if action[idx]==3:
+                parameter_value[idx,2] = parameter_value[idx,1]*0.9
+            if action[idx]==4:
+                parameter_value[idx,2] = parameter_value[idx,1] *0.5
     
     current_patch = np.zeros((Patch_num, PATCH_SIZE[0], PATCH_SIZE[0]))
-
+    """
     # noise removal for each patch
     for  idx in range(Patch_num):
-        basic_patch = Bm3d.BM3D_1st_step(state[idx,:,:],parameter_value[idx,0], parameter_value[idx,1]) 
+        basic_patch = Bm3d.BM3D_1st_step(original_image, parameter_value[idx,0], parameter_value[idx,1]) 
         final_patch = Bm3d.BM3D_2nd_step(basic_patch, state[idx,:,:], parameter_value[idx,0], parameter_value[idx,1]) 
         current_patch [idx, :, :] = final_patch
-        
-    #############
+    """
+    org_patch = divide_patch(original_image)
+    for  idx in range(Patch_num):
+        final_patch = NLM.NLM(org_patch[idx,:,:], parameter_value[idx,0], parameter_value[idx,1],parameter_value[idx,2]) 
+        current_patch [idx, :, :] = final_patch
+    ############# NLM
 
     ### how to stitch  256x 256 patches into a single image fimg  after Bm3d
-    fimg = np.reshape(current_patch[:, int(PATCH_SIZE[0]//2), int(PATCH_SIZE[0]//2)], (RESOLUTION, RESOLUTION), order='F')
+    next_img = np.reshape(current_patch[:, int(PATCH_SIZE[0]//2), int(PATCH_SIZE[0]//2)], (RESOLUTION, RESOLUTION), order='F')
     next_state = current_patch
-
+    current_image = np.reshape(state[:, int(PATCH_SIZE[0]//2), int(PATCH_SIZE[0]//2)], (RESOLUTION, RESOLUTION), order='F')
      #next_state = divid_patch(fimg)
 
     #############   calculate reward and error
     #dist1 = np.reshape(original_image-GroundTruth,(Patch_num),order='F')
 
-    dist1img = original_image-GroundTruth
-    dist2img = fimg-GroundTruth
+    dist1img = current_image - GroundTruth
+    dist2img = next_img - GroundTruth  #######################################
     dist2 = np.reshape(dist2img, (Patch_num), order='F')
 
     dist1imgLarge = np.zeros((RESOLUTION+PATCH_reward-1,RESOLUTION+PATCH_reward-1))
@@ -143,13 +165,15 @@ def Denoise(state, parameter, action, parameter_value, GroundTruth, original_ima
     for i in range(RESOLUTION):
         for j in range(RESOLUTION):
             temp = np.sum(dist1imgLarge[j:j + PATCH_reward, i:i + PATCH_reward]) - np.sum(dist2imgLarge[j:j + PATCH_reward, i:i + PATCH_reward])
-            
+
+            ### when reach the ground truth 
             if np.sum(dist1imgLarge[j:j+PATCH_reward,i:i+PATCH_reward])==0:
                 if temp==0:
                     reward[count]=1
                 else:
                     reward[count]=-1
                 count += 1
+            ### not yet get the best result
             else:
                 factor = 0.005
                 if temp/np.sum(dist1imgLarge[j:j+PATCH_reward,i:i+PATCH_reward])>=factor:
@@ -168,13 +192,16 @@ def Denoise(state, parameter, action, parameter_value, GroundTruth, original_ima
     error = np.sum(np.absolute(dist2))
 
 
-    return next_state, reward, parameter_value, fimg ,error
+    return next_state, reward, parameter_value, next_img ,error
 
 def main():
 
-    train_data = skimage.external.tifffile.imread("C:/Users/ZhenjuYin/Downloads/a1.tif")
-    TrueImgTrain = skimage.external.tifffile.imread("C:/Users/ZhenjuYin/Downloads/t.tif")
-    datasize = train_data.shape
+    #train_data = skimage.external.tifffile.imread("C:/Users/ZhenjuYin/Downloads/a1.tif")
+    TrueImgTrain = skimage.external.tifffile.imread("C:/Users/ZhenjuYin/Downloads/G.tif")
+    datasize = TrueImgTrain.shape
+    gaussian = np.random.normal(0.3, 0.9, (datasize[0],datasize[1],datasize[2])) 
+    train_data = TrueImgTrain + gaussian
+    
     # (None, channel, H, w, depth) for Volume
     mainDQN = RL.doubleDQN(PATCH_SIZE[0], PATCH_SIZE[1], len(Para),len(Actions))
     targetDQN = RL.doubleDQN(PATCH_SIZE[0], PATCH_SIZE[1], len(Para),len(Actions))
@@ -186,24 +213,27 @@ def main():
     done_sel = np.zeros((REPLAY_MEMORY))
     para_sel = np.zeros((REPLAY_MEMORY))
     #value_sel = np.zeros((REPLAY_MEMORY, len(Para)))
-
-    if MAX_EPOCHS>0:
+    indicator = 0 
+    if MAX_EPOCHS>0:  ########################## delete
                    
             State = np.zeros((datasize[0], Patch_num, PATCH_SIZE[0],PATCH_SIZE[0]))             ##  slabs  x  256**2 x 9 x 9
             parameter_value = np.zeros((Patch_num, len(Para)))
             parameter_value[:,0] = 1.5                                 # sigma at first step
-            parameter_value[:,1] = 2.0                                 # Beta_kasier
-     
+            parameter_value[:,1] = 1.0                                 # Beta_kasier
+            parameter_value[:,2] = 2.0 
             # for each image initializer
             for IMG in range(datasize[0]):
-                state = divid_patch(  train_data[ IMG,:,:]  )  ##  256**2 x 9 x 9              
+                state = divide_patch(  train_data[ IMG,:,:]  )  ##  256**2 x 9 x 9              
                 GroundTruth = TrueImgTrain[IMG,:, : ]      
                 ## initialize the 1st and 2nd paths 
                 parameter = np.ones((Patch_num))
                 action = 2 * np.ones((Patch_num))
-                next_state, reward, parameter_value, img, error = Denoise( state,  parameter, action, parameter_value , GroundTruth, train_data[ IMG,:,:] )
-                State[ IMG, :, :, :] = next_state
+                #next_state, reward, parameter_value, img, error = Denoise( state,  parameter, action, parameter_value , GroundTruth, train_data[ IMG,:,:] )
+                State[ IMG, :, :, :] = state
+                #psnr = PSNR.PSNR(train_data[IMG,:,:], img )
+                #print(" current PSNR : {}".format(psnr))
                 print(IMG)
+
 
             State_initial = State
             count_memory = 0
@@ -246,11 +276,10 @@ def main():
                         y1, y2 = mainDQN(yy)
                         parameter_yy = torch.argmax(y1, axis=1)
                         action_yy = torch.argmax(y2, axis=1)
-                        #print(parameter_yy)
-                        #print(action_yy)
+                        
 
-                        avg_action = action_yy.type(torch.float32).mean()
-                        avg_para = parameter_yy.type(torch.float32).mean()
+                        avg_action = action_yy.type(torch.float64).mean()
+                        avg_para = parameter_yy.type(torch.float64).mean()
                         print('average action taken is: {} average parameter taken is: {}'.format(avg_action,avg_para))
                         
                         #### action and paramter chosen
@@ -261,9 +290,11 @@ def main():
                                 parameter[idx] = parameter_yy[count_patch]
                                 count_patch += 1
 
-                        original = np.reshape(state[:, int(PATCH_SIZE[0]//2), int(PATCH_SIZE[0]//2)], (RESOLUTION, RESOLUTION), order='F')
-                        next_state, reward, parameter_value, img, error = Denoise( state,  parameter, action,parameter_value,  GroundTruth, original )
-                     
+                        #original = np.reshape(state[:, int(PATCH_SIZE[0]//2), int(PATCH_SIZE[0]//2)], (RESOLUTION, RESOLUTION), order='F')
+                        next_state, reward, parameter_value, img, error = Denoise( state,  parameter, action,parameter_value,  GroundTruth, train_data[ IMG_IDX,:,:] )
+                        psnr = PSNR.PSNR(train_data[IMG_IDX,:,:], img )
+                        print(" current PSNR : {}".format(psnr))
+                        """
                         fig, axs = plt.subplots(1, 3)
                         fig.suptitle('current results Bm3d')
                         axs[ 0].imshow(train_data[ IMG_IDX,:,:], cmap='gray')
@@ -273,11 +304,8 @@ def main():
                         axs[ 0].set_title('original')
                         axs[ 1].set_title('Filtered')
                         axs[ 2].set_title('Truth')
-
-                        #plt.imshow(np.reshape(GroundTruth, (RESOLUTION, RESOLUTION), order='F'))
-                        plt.show(block=False)
-                        plt.pause(0.1)
-
+                        #plt.show()
+                        """
                         ###################  ? random replacement
                         sel_prob = 0.01
                         flag1 = np.random.rand(Patch_num)
@@ -349,16 +377,11 @@ def main():
                                 step_count += 1
 
                         State[IMG_IDX, :, :, :] = next_state
-                        psnr = PSNR.PSNR(train_data[IMG_IDX,:,:], img )
-                        print(" current PSNR : {}".format(psnr))
+                        
 
                     print("Episode: {}  Iterations: {} Loss: {}".format(episode, ITER_NUM, loss))
 
                 CHECK = episode+1
-
-  
-
-
 
 if __name__ == '__main__':
     main()
